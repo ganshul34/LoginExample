19317ee0e083f8a30a5379fd534d6325534f74e2
Anshul <ganshul34@gmail.com>
2019-12-31 07:49:16 +0530

    Updated Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 
 export default class Home extends Component {
-    render(){
-        return(
-            <div className="App">
-                Home Screen
-            </div>
+    render() {
+        return (
+            <div className="Home" style={{ textAlign: 'center' }}>
+				<h1 >Home</h1>
+                <h2> Lorem Ipsum</h2>
+		    </div>			
         )
     }
 }
