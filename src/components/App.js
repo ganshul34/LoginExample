18d9c840ac297a1ca94f304d72282b247427e0ea
Anshul <ganshul34@gmail.com>
2019-12-31 05:54:56 +0530

    Initial Code

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+
+
+class App extends Component {
+	render() {
+		return (
+					
+            <div className="App">
+					
+			</div>
+				
+		);
+	}
+}
+export default App;
