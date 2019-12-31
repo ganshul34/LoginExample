b628cda8fac706b65df0df5cc3305bd3fdeb713a
Anshul <ganshul34@gmail.com>
2019-12-31 06:28:17 +0530

    Add login screen logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+ 
+import Login from './Login'
 
 
 class App extends Component {
 	render() {
 		return (
-					
-            <div className="App">
-					
-			</div>
+			<Router>	
+                <div className="App">
+                   <Switch>
+						<Route exact path="/register" component={null} />
+						<Route exact path="/login" component={Login} />
+						<Redirect from="/" to="login" />
+					</Switch>     
+                </div>
+            </Router>
+           
 				
 		);
 	}
